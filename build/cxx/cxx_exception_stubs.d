2b884cb0907cbd676fc62bb3d5102809fbd5d11c
Manish <manish@parkzap.com>
2018-12-31 04:15:18 +0530

    initial commit

---
+++ build/cxx/cxx_exception_stubs.d
@@ -0,0 +1,6 @@
+cxx_exception_stubs.o: \
+ /home/manish/esp/esp-idf/components/cxx/cxx_exception_stubs.cpp \
+ /home/manish/github_repo/esp32_remote_log_client/build/include/sdkconfig.h
+
+/home/manish/github_repo/esp32_remote_log_client/build/include/sdkconfig.h:
+/home/manish/esp/esp-idf/components/cxx/./cxx_exception_stubs.cpp:
